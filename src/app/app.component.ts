47684f4d0c07cb144efc0e291fecebce2be5005f
Brad Bunce <76120543+bradbunce@users.noreply.github.com>
2024-02-15 15:04:39 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as LDClient from 'launchdarkly-js-client-sdk';
+
+const context: LDClient.LDContext = {
+  "kind": "multi",
+  "user": {
+    "key": "user-key",
+    "name": "User",
+    "email": "user@organization.com"
+    },
+  "group": {
+    "key": "group",
+    "name": "Group",
+    "address": {
+      "street": "1999 Harrison St",
+      "city": "Oakland"
+      }
+    }
+};
+
+const client = LDClient.initialize('Client-side ID', context);
+
+client.waitForInitialization().then(() => {
+  // initialization succeeded, flag values are now available
+  const boolFlagValue = client.variation('angular-test-flag', false) as boolean;
+  console.log(`The feature flag variation is: ${boolFlagValue}`);
+}).catch((err: Error) => {
+  // initialization failed
+});
+
+client.on('change', (settings: any) => {
+  const boolFlagValue = client.variation('angular-test-flag') as boolean;
+  console.log(`The feature flag variation is now: ${boolFlagValue}`);
+});
 
 @Component({
   selector: 'app-root',
